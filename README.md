dc50be92650281e369d88aebeab742433f4f5d98
bill-mark <2286449127@qq.com>
2016-11-26 09:25:03 +0800

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# da-jian-kang-web
-从前端到后端的大健康生活网项目，技术栈：前端：H5，css3，jq；后端：node,koa
--- js/nav.js
+++ js/nav.js
@@ -6,3 +6,18 @@ function hideSubMenu(li) {
 	var subMenu = li.getElementsByTagName("ul")[0]; 
 	subMenu.style.display = "none"; 
 } 
+
+
+function changeweb(obj,id){
+         //obj.className="in-li2"; //更换点击后背景色
+         var iframe = document.getElementById('iframe');
+         var chg = id+'.html';  //拼接html文件    
+         iframe.setAttribute("src",chg); //设置对象的属性        
+}
+function changeweb2(obj,id){
+         //window.loaction.href="index.html";
+         scrollTo(0,0);
+         var iframe = document.getElementById('iframe');
+         var chg = id+'.html';  //拼接html文件    
+         iframe.setAttribute("src",chg); //设置对象的属性        
+}
